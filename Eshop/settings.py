c5af1b7fc8e3c773de960a59ae1df95abc6da6a0
saikap2009@gmail.com <Team@1976>
2022-05-17 07:52:56 +0200

    commit

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = "/images/downloads/"
+
+MEDIA_ROOT = BASE_DIR
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
